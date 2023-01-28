82614f7ed86826ea6d726ab494e65b934f044d5b
JorgeEduardo96 <jorgeeduardojr96@gmail.com>
2023-01-28 10:52:52 +0000

    Criaçaõ da navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,7 +3,8 @@ import { CommonModule, DatePipe, registerLocaleData } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import localePt from '@angular/common/locales/pt';
 
-import { MessageService } from 'primeng/api';
+import { MessageService, MenuItem } from 'primeng/api';
+import {MenubarModule} from 'primeng/menubar';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { AuthService } from '../seguranca/auth.service';
@@ -22,11 +23,13 @@ registerLocaleData(localePt, 'pt-BR');
   ],
   imports: [
     CommonModule,
+    MenubarModule,
 
     TranslateModule.forRoot(),
   ],
   exports: [
-    MessageComponent
+    MessageComponent,
+    NavbarComponent
   ],
   providers: [
     MessageService,
